99d5b4ddf6407e1f3b25061a174ffffe9d212e45
jheffh2012 <jheffh2012@gmail.com>
2016-11-22 23:26:54 -0200

    Ajustes na Composição de Meio de Cultura

--- uComposicaoMeioCultura.pas
+++ uComposicaoMeioCultura.pas
@@ -13,15 +13,10 @@ uses
 type
   TfrmComposicaoMeioCultura = class(TForm)
     Panel2: TPanel;
-    gbMeioCultura: TGroupBox;
-    edtMeioCultura: TButtonedEdit;
-    edtNomeMeioCultura: TEdit;
     GroupBox1: TGroupBox;
     edtMateriaPrima: TButtonedEdit;
     edtNomeMateriaPrima: TEdit;
     edt_Quantidade: TJvValidateEdit;
-    Label1: TLabel;
-    Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
     Label5: TLabel;
@@ -38,16 +33,32 @@ type
     dg_Componentes: TDBGrid;
     ds_Componentes: TDataSource;
     cds_ComponentesID: TIntegerField;
-    btBuscaComponentes: TBitBtn;
     btNovo: TBitBtn;
     btExcluir: TBitBtn;
-    procedure edtMeioCulturaRightButtonClick(Sender: TObject);
+    pnCadastro: TPanel;
+    pnPesquisa: TPanel;
+    cds_Pesquisa: TClientDataSet;
+    cds_PesquisaID: TIntegerField;
+    cds_PesquisaDESCRICAO: TStringField;
+    cds_PesquisaFINALIDADE: TIntegerField;
+    cds_PesquisaUNIDADEMEDIDA: TIntegerField;
+    cds_PesquisaCODIGOEXTERNO: TStringField;
+    ds_Pesquisa: TDataSource;
+    pnPequisaDados: TPanel;
+    btPesquisar: TSpeedButton;
+    edPesquisa: TEdit;
+    gdPesquisa: TDBGrid;
+    pnBotoesVisualizacao: TPanel;
+    gpBotoes: TGridPanel;
+    Panel8: TPanel;
+    btAlterar: TSpeedButton;
+    Panel9: TPanel;
+    btFechar: TSpeedButton;
+    btExportar: TSpeedButton;
     procedure edtMeioCulturaKeyDown(Sender: TObject; var Key: Word;
       Shift: TShiftState);
-    procedure edtMeioCulturaChange(Sender: TObject);
     procedure btNovoClick(Sender: TObject);
     procedure btExcluirClick(Sender: TObject);
-    procedure btBuscaComponentesClick(Sender: TObject);
     procedure edtMateriaPrimaRightButtonClick(Sender: TObject);
     procedure edtMateriaPrimaKeyDown(Sender: TObject; var Key: Word;
       Shift: TShiftState);
@@ -57,14 +68,22 @@ type
     procedure FormCreate(Sender: TObject);
     procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
     procedure FormShow(Sender: TObject);
+    procedure cds_PesquisaFilterRecord(DataSet: TDataSet; var Accept: Boolean);
+    procedure btPesquisarClick(Sender: TObject);
+    procedure btAlterarClick(Sender: TObject);
+    procedure btExportarClick(Sender: TObject);
+    procedure btFecharClick(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
-    procedure SelecionaMeioCultura;
     procedure SelecionaMateriaPrima;
     procedure SelecionaComponentes;
     procedure GravarComponentes;
+    procedure CarregaDados;
+    procedure Filtrar;
+    procedure InvertePaineis;
+    procedure Cancelar;
   end;
 
 var
@@ -83,23 +102,39 @@ uses
 
 { TfrmComposicaoMeioCultura }
 
-procedure TfrmComposicaoMeioCultura.btBuscaComponentesClick(Sender: TObject);
+procedure TfrmComposicaoMeioCultura.btAlterarClick(Sender: TObject);
 begin
+  InvertePaineis;
+  pnCadastro.Tag := cds_PesquisaID.Value;
   SelecionaComponentes;
-  btNovo.Enabled    := True;
-  btExcluir.Enabled := True;
-  btGravar.Enabled  := True;
 end;
 
 procedure TfrmComposicaoMeioCultura.btCancelarClick(Sender: TObject);
 begin
-  Close;
+  Cancelar;
 end;
 
 procedure TfrmComposicaoMeioCultura.btExcluirClick(Sender: TObject);
 begin
   if not cds_Componentes.IsEmpty then
     cds_Componentes.Delete;
+end;
+
+procedure TfrmComposicaoMeioCultura.btExportarClick(Sender: TObject);
+begin
+  if btExportar.Tag = 0 then begin
+    btExportar.Tag := 1;
+    try
+      ExpXLS(cds_Pesquisa, Caption + '.xlsx');
+    finally
+      btExportar.Tag := 0;
+    end;
+  end;
+end;
+
+procedure TfrmComposicaoMeioCultura.btFecharClick(Sender: TObject);
+begin
+  Close;
 end;
 
 procedure TfrmComposicaoMeioCultura.btGravarClick(Sender: TObject);
@@ -128,6 +163,80 @@ begin
   end;
 end;
 
+procedure TfrmComposicaoMeioCultura.btPesquisarClick(Sender: TObject);
+begin
+  Filtrar;
+end;
+
+procedure TfrmComposicaoMeioCultura.Cancelar;
+begin
+  cds_Componentes.EmptyDataSet;
+  pnCadastro.Tag := 0;
+  InvertePaineis;
+end;
+
+procedure TfrmComposicaoMeioCultura.CarregaDados;
+Var
+  FWC : TFWConnection;
+  P   : TPRODUTO;
+  I,
+  Codigo  : Integer;
+begin
+
+  try
+    FWC := TFWConnection.Create;
+    P   := TPRODUTO.Create(FWC);
+    cds_Pesquisa.DisableControls;
+    try
+
+      Codigo := cds_PesquisaID.Value;
+
+      cds_Pesquisa.EmptyDataSet;
+
+      P.SelectList('ID > 0 AND FINALIDADE = 3', 'ID');
+      if P.Count > 0 then begin
+        for I := 0 to P.Count -1 do begin
+          cds_Pesquisa.Append;
+          cds_PesquisaID.Value              := TPRODUTO(P.Itens[I]).ID.Value;
+          cds_PesquisaDESCRICAO.Value       := TPRODUTO(P.Itens[I]).DESCRICAO.Value;
+          cds_PesquisaFINALIDADE.Value      := TPRODUTO(P.Itens[I]).FINALIDADE.Value;
+          cds_PesquisaUNIDADEMEDIDA.Value   := TPRODUTO(P.Itens[I]).UNIDADEMEDIDA_ID.Value;
+          cds_PesquisaCODIGOEXTERNO.Value   := TPRODUTO(P.Itens[I]).CODIGOEXTERNO.Value;
+          cds_Pesquisa.Post;
+        end;
+      end;
+
+      if Codigo > 0 then
+        cds_Pesquisa.Locate('ID', Codigo, []);
+    except
+      on E : Exception do begin
+        DisplayMsg(MSG_ERR, 'Erro ao Carregar os dados da Tela.', '', E.Message);
+      end;
+    end;
+
+  finally
+    cds_Pesquisa.EnableControls;
+    FreeAndNil(P);
+    FreeAndNil(FWC);
+  end;
+end;
+
+procedure TfrmComposicaoMeioCultura.cds_PesquisaFilterRecord(DataSet: TDataSet;
+  var Accept: Boolean);
+Var
+  I : Integer;
+begin
+  Accept := False;
+  for I := 0 to DataSet.Fields.Count - 1 do begin
+    if not DataSet.Fields[I].IsNull then begin
+      if Pos(AnsiLowerCase(edPesquisa.Text),AnsiLowerCase(DataSet.Fields[I].AsVariant)) > 0 then begin
+        Accept := True;
+        Break;
+      end;
+    end;
+  end;
+end;
+
 procedure TfrmComposicaoMeioCultura.edtMateriaPrimaChange(Sender: TObject);
 begin
   edtMateriaPrima.Tag := 0;
@@ -148,27 +257,18 @@ begin
   SelecionaMateriaPrima;
 end;
 
-procedure TfrmComposicaoMeioCultura.edtMeioCulturaChange(Sender: TObject);
-begin
-  edtMeioCultura.Tag := 0;
-  edtNomeMeioCultura.Clear;
-  btNovo.Enabled    := False;
-  btExcluir.Enabled := False;
-  btGravar.Enabled  := False;
-end;
-
 procedure TfrmComposicaoMeioCultura.edtMeioCulturaKeyDown(Sender: TObject;
   var Key: Word; Shift: TShiftState);
 begin
-  case Key of
-    VK_RETURN : SelecionaMeioCultura;
-  end;
+//  case Key of
+//    VK_RETURN : SelecionaMeioCultura;
+//  end;
 end;
 
-procedure TfrmComposicaoMeioCultura.edtMeioCulturaRightButtonClick(
-  Sender: TObject);
+procedure TfrmComposicaoMeioCultura.Filtrar;
 begin
-  SelecionaMeioCultura;
+  cds_Pesquisa.Filtered := False;
+  cds_Pesquisa.Filtered := Length(edPesquisa.Text) > 0;
 end;
 
 procedure TfrmComposicaoMeioCultura.FormCreate(Sender: TObject);
@@ -181,13 +281,50 @@ end;
 procedure TfrmComposicaoMeioCultura.FormKeyDown(Sender: TObject; var Key: Word;
   Shift: TShiftState);
 begin
-  if Key = VK_ESCAPE then
-    btCancelarClick(nil);
+  if pnPesquisa.Visible then begin
+    case Key of
+      VK_ESCAPE : Close;
+      VK_RETURN : begin
+        if edPesquisa.Focused then begin
+          Filtrar;
+        end else begin
+          if edPesquisa.CanFocus then begin
+            edPesquisa.SetFocus;
+            edPesquisa.SelectAll;
+          end;
+        end;
+      end;
+      VK_F5 : CarregaDados;
+      VK_UP : begin
+        if not cds_Pesquisa.IsEmpty then begin
+          if cds_Pesquisa.RecNo > 1 then
+            cds_Pesquisa.Prior;
+        end;
+      end;
+      VK_DOWN : begin
+        if not cds_Pesquisa.IsEmpty then begin
+          if cds_Pesquisa.RecNo < cds_Pesquisa.RecordCount then
+            cds_Pesquisa.Next;
+        end;
+      end;
+    end;
+  end else begin
+    case Key of
+      VK_ESCAPE : Cancelar;
+    end;
+  end;
 end;
 
 procedure TfrmComposicaoMeioCultura.FormShow(Sender: TObject);
 begin
+  pnPesquisa.Visible := True;
+  pnCadastro.Visible := not pnPesquisa.Visible;
   AutoSizeDBGrid(dg_Componentes);
+  AutoSizeDBGrid(gdPesquisa);
+  cds_Pesquisa.CreateDataSet;
+  cds_Pesquisa.Open;
+
+  CarregaDados;
 end;
 
 procedure TfrmComposicaoMeioCultura.GravarComponentes;
@@ -202,7 +339,7 @@ begin
     DisplayMsg(MSG_WAIT, 'Gravando dados no banco de dados...');
     FWC.StartTransaction;
     try
-      PC.SelectList('id_produto = ' + QuotedStr(edtMeioCultura.Text));
+      PC.SelectList('id_produto = ' + IntToStr(pnCadastro.Tag));
       for I := 0 to Pred(PC.Count) do begin
         if not cds_Componentes.Locate(cds_ComponentesID.FieldName, TPRODUTOCOMPOSICAO(PC.Itens[I]).ID.Value, []) then begin
           PC.ID.Value := TPRODUTOCOMPOSICAO(PC.Itens[I]).ID.Value;
@@ -216,7 +353,7 @@ begin
         PC.QUANTIDADE.Value    := cds_ComponentesQUANTIDADE.Value;
         if cds_ComponentesID.IsNull then begin
           PC.ID.isNull         := True;
-          PC.ID_PRODUTO.Value  := StrToInt(edtMeioCultura.Text);
+          PC.ID_PRODUTO.Value  := pnCadastro.Tag;
           PC.Insert;
         end else begin
           PC.ID.Value          := cds_ComponentesID.Value;
@@ -226,10 +363,9 @@ begin
       end;
       FWC.Commit;
 
-      edtMeioCultura.Clear;
       cds_Componentes.EmptyDataSet;
-      if edtMeioCultura.CanFocus then edtMeioCultura.SetFocus;
       DisplayMsgFinaliza;
+      InvertePaineis;
     except
       on E : Exception do begin
         FWC.Rollback;
@@ -239,6 +375,18 @@ begin
   finally
     FreeAndNil(PC);
     FreeAndNil(FWC);
+  end;
+end;
+
+procedure TfrmComposicaoMeioCultura.InvertePaineis;
+begin
+  pnPesquisa.Visible            := not pnPesquisa.Visible;
+  pnBotoesVisualizacao.Visible  := pnPesquisa.Visible;
+  pnCadastro.Visible            := not pnCadastro.Visible;
+  pnBotoesEdicao.Visible        := pnCadastro.Visible;
+  if pnCadastro.Visible then begin
+    if edtMateriaPrima.CanFocus then
+      edtMateriaPrima.SetFocus;
   end;
 end;
 
@@ -252,17 +400,17 @@ begin
   SQL := TFDQuery.Create(nil);
   try
     cds_Componentes.EmptyDataSet;
-    if edtMeioCultura.Text = EmptyStr then begin
-      DisplayMsg(MSG_INF, 'Informe um meio de cultura para continuar!');
-      Exit;
-    end;
+//    if edtMeioCultura.Text = EmptyStr then begin
+//      DisplayMsg(MSG_INF, 'Informe um meio de cultura para continuar!');
+//      Exit;
+//    end;
     DisplayMsg(MSG_WAIT, 'Consultando componentes no banco de dados!');
     try
       SQL.Close;
       SQL.SQL.Clear;
       SQL.Connection := FWC.FDConnection;
       SQL.SQL.Add('SELECT p.id, p.descricao, pc.quantidade, pc.id FROM produto p INNER JOIN produtocomposicao pc ON p.id = pc.id_componente WHERE pc.id_produto = :PRODUTO');
-      SQL.ParamByName('PRODUTO').AsInteger := StrToInt(edtMeioCultura.Text);
+      SQL.ParamByName('PRODUTO').AsInteger := pnCadastro.Tag;
       SQL.Prepare;
       SQL.Open();
 
@@ -306,32 +454,7 @@ begin
       if P.Count > 0 then begin
         edtMateriaPrima.Text     := TPRODUTO(P.Itens[0]).ID.asString;
         edtNomeMateriaPrima.Text := TPRODUTO(P.Itens[0]).DESCRICAO.asString;
-        if edt_Quantidade.CanFocus then edt_Quantidade.SetFocus;        
-      end;
-    end;
-  finally
-    FreeAndNil(P);
-    FreeAndNil(FWC);
-  end;
-end;
-
-procedure TfrmComposicaoMeioCultura.SelecionaMeioCultura;
-var
-  FWC : TFWConnection;
-  P   : TPRODUTO;
-  Filtro : string;
-begin
-  FWC    := TFWConnection.Create;
-  P      := TPRODUTO.Create(FWC);
-  edtNomeMeioCultura.Clear;
-  try
-    Filtro := 'finalidade = 3';
-    edtMeioCultura.Tag := DMUtil.Selecionar(P, edtMeioCultura.Text, Filtro);
-    if edtMeioCultura.Tag > 0 then begin
-      P.SelectList('id = ' + IntToStr(edtMeioCultura.Tag));
-      if P.Count > 0 then begin
-        edtMeioCultura.Text     := TPRODUTO(P.Itens[0]).ID.asString;
-        edtNomeMeioCultura.Text := TPRODUTO(P.Itens[0]).DESCRICAO.asString;
+        if edt_Quantidade.CanFocus then edt_Quantidade.SetFocus;
       end;
     end;
   finally
