46b87e60a6128cfa2a5d2ac54ffb731bbb40080c
jheffh2012 <jheffh2012@gmail.com>
2017-08-16 20:30:19 -0300

    Ajuste localização

--- Beans/uBeanOPFinal_Estagio_Lote_S_Positivo.pas
+++ Beans/uBeanOPFinal_Estagio_Lote_S_Positivo.pas
@@ -12,20 +12,20 @@ type
     FOBSERVACAO: TFieldString;
     FID_OPFINAL_ESTAGIO_LOTE_S: TFieldInteger;
     FID: TFieldInteger;
-    FLOCALIZACAO: TFieldString;
     FID_IMAGEM: TFieldInteger;
+    FLOCALIZACAO_ID: TFieldInteger;
     procedure SetID(const Value: TFieldInteger);
     procedure SetID_IMAGEM(const Value: TFieldInteger);
     procedure SetID_OPFINAL_ESTAGIO_LOTE_S(const Value: TFieldInteger);
-    procedure SetLOCALIZACAO(const Value: TFieldString);
     procedure SetOBSERVACAO(const Value: TFieldString);
+    procedure SetLOCALIZACAO_ID(const Value: TFieldInteger);
   protected
     procedure InitInstance; override;
   published
     property ID : TFieldInteger read FID write SetID;
     property ID_OPFINAL_ESTAGIO_LOTE_S : TFieldInteger read FID_OPFINAL_ESTAGIO_LOTE_S write SetID_OPFINAL_ESTAGIO_LOTE_S;
     property ID_IMAGEM : TFieldInteger read FID_IMAGEM write SetID_IMAGEM;
-    property LOCALIZACAO : TFieldString read FLOCALIZACAO write SetLOCALIZACAO;
+    property LOCALIZACAO_ID : TFieldInteger read FLOCALIZACAO_ID write SetLOCALIZACAO_ID;
     property OBSERVACAO : TFieldString read FOBSERVACAO write SetOBSERVACAO;
   end;
 implementation
@@ -37,7 +37,6 @@ begin
   inherited;
   ID.isPK := True;
 
-  LOCALIZACAO.Size := 100;
   OBSERVACAO.Size  := 500;
 end;
 
@@ -58,10 +57,10 @@ begin
   FID_OPFINAL_ESTAGIO_LOTE_S := Value;
 end;
 
-procedure TOPFINAL_ESTAGIO_LOTE_S_POSITIVO.SetLOCALIZACAO(
-  const Value: TFieldString);
+procedure TOPFINAL_ESTAGIO_LOTE_S_POSITIVO.SetLOCALIZACAO_ID(
+  const Value: TFieldInteger);
 begin
-  FLOCALIZACAO := Value;
+  FLOCALIZACAO_ID := Value;
 end;
 
 procedure TOPFINAL_ESTAGIO_LOTE_S_POSITIVO.SetOBSERVACAO(
